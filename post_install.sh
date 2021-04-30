fc37578b88f36ded06f3a62de00bc509ca86d6c4
fuji44 <fuji.44mt@gmail.com>
2021-05-01 05:25:37 +0900

    Improved the installation process

--- post_install.sh
+++ post_install.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-# Create run user
-pw useradd scrapy -s /bin/csh -m
-
 # Install scrapyd
 : ${scrapyd_app_dir="/usr/local/scrapyd"}
-sudo -H -u scrapy /usr/local/bin/python3.9 -m venv $scrapyd_app_dir
-sudo -H -u scrapy $scrapyd_app_dir/bin/python3.9 -m pip install --upgrade pip
-sudo -H -u scrapy $scrapyd_app_dir/bin/pip install scrapyd
+/usr/local/bin/python3.9 -m venv $scrapyd_app_dir
+$scrapyd_app_dir/bin/python3.9 -m pip install --upgrade pip
+$scrapyd_app_dir/bin/pip install scrapyd
+
+# Create run user
+pw useradd scrapy -s /bin/csh -m
 chown -R scrapy $scrapyd_app_dir
-echo 'source $HOME/scrapyd/bin/activate.csh' >> /home/scrapy/.cshrc
+echo "source $scrapyd_app_dir/bin/activate.csh" >> /home/scrapy/.cshrc
 
 # Configure scrapyd
 sysrc -f /etc/rc.conf scrapyd_enable="YES"
-sysrc -f /etc/rc.conf scrapyd_app_dir=$scrapyd_app_dir
-sysrc -f /etc/rc.conf scrapyd_user=scrapy
+sysrc -f /etc/rc.conf scrapyd_app_dir="$scrapyd_app_dir"
+sysrc -f /etc/rc.conf scrapyd_user="scrapy"
 
 service scrapyd start
 
-echo "✅ flexget installation is complete!" > /root/PLUGIN_INFO
+echo "✅ scrapyd installation is complete!" > /root/PLUGIN_INFO
+echo "App dir: $scrapyd_app_dir" >> /root/PLUGIN_INFO
+echo "Config: /etc/scrapyd/scrapyd.conf" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "The console will be output to '/tmp/scrapyd.log'. (Different from normal logs)" >> /root/PLUGIN_INFO
